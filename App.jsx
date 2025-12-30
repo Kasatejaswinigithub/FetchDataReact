82f63e170bf486c6cb6d6e6e939537f00ec8ff41
Kasatejaswinigithub <157687507+Kasatejaswinigithub@users.noreply.github.com>
2025-12-30 17:20:29 +0530

    Create App component with Fetch integration

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Fetch from './propsContainer/Fetch'
+function App() {
+  return (
+    <div className="container">
+    <h1>welcome to React</h1>
+    <Fetch></Fetch>
+    </div>
+  );
+}
+
+export default App;
